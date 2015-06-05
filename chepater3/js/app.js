24484ac0a95c44ce770625b5abefe4b0ac48484f
Sreehari Inukollu <hari.inukollu@ttu.edu>
2015-06-05 17:14:21 -0500

    Ook

---
+++ chepater3/js/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by sinukoll on 4/15/15.
+ */
+(function(angular) {
+'use strict';
+ var spl_app = angular.module('spl_app',['spl_app_controller','spl_app_service']);
+ })(window.angular);
+
+
+
+
+
